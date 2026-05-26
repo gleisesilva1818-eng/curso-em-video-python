7def115c01ffb9cee57221b0df4d2edde525c12e
Gleise Silva <gleise.silva1818@gmail.com>
2026-05-25 21:47:36 -0300

    Add files via upload

---
+++ Mundo 1/ex010.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar
+# Considere US$1,00 = R$ 3,27
+
+reais = float(input('Quanto dinheiro você tem na carteira? R$ '))
+dolar = reais / 3.27
+print(f'Com R$ {reais:.2f}, você pode comprar US$ {dolar:.2f}')
