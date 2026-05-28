15d1812c65705c23c9e751ae8cd8d908cf58b895
Gleise Silva <gleise.silva1818@gmail.com>
2026-05-27 22:31:45 -0300

    Add files via upload

---
+++ Mundo 1/ex013.py
@@ -0,0 +1,5 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento
+
+salário = float(input('Qual é o salário do funcionário? R$'))
+aumento = salário + (salário * 15 / 100)
+print(f'Um funcionário que ganhava R${salário:.2f}, com 15% de aumento, passa a receber R${aumento:.2f}')
