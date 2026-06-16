6b97138788b54c037006ade42bfab6b70969a96f
Gleise Silva <gleise.silva1818@gmail.com>
2026-06-16 19:36:01 -0300

    Delete ex021.py

--- ex021.py
+++
@@ -1,7 +0,0 @@
-# Faça um programa em Python que reproduza o áudio de um arquivo MP3:
-
-import pygame 
-pygame.init()
-pygame.mixer.music.load('ex021.mp3')
-pygame.mixer.music.play()
-input('Música tocando! Aperte Enter para pausar...')
