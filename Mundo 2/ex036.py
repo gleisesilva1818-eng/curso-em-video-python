8d543229ca47600bd2f35f52bab481ecad6521b7
Gleise Silva <gleise.silva1818@gmail.com>
2026-07-13 20:29:46 -0300

    Create ex036.py

---
+++ Mundo 2/ex036.py
@@ -0,0 +1,14 @@
+# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa
+# O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+# Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
+
+casa = float(input('Valor da casa: R$'))
+salário = float(input('Salário do comprador: R$'))
+anos = int(input('Quantos anos de financiamento? '))
+prestação = casa / (anos * 12)
+mínimo = salário * 30 / 100
+print(f'Para pagar uma casa de R${casa:.2f} em {anos} anos, a prestação será de R${prestação:.2f}.')
+if prestação <= mínimo:
+    print('Empréstimo pode ser CONCEDIDO!')
+else:
+    print('Empréstimo NEGADO!')
