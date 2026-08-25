7f6de3296e7cfe9a2654138319f277dfd51eac40
Gleise Silva <gleise.silva1818@gmail.com>
2026-08-24 21:11:51 -0300

    Ex 61 finalizado

--- Mundo 2/ex061.py
+++ Mundo 2/ex061.py
@@ -1,3 +1,15 @@
 # Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a
 # estrutura while.
 
+print('GERADOR DE PA')
+print('--=--' * 12)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print(f'{termo} - ', end= '')
+    termo += razão
+    cont += 1
+print('FIM')
+print('--=--' * 12)
