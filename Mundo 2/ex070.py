8d5a406f555c4ada8d08b39c9cc5f5c2ff86b27f
Gleise Silva <gleise.silva1818@gmail.com>
2026-09-24 22:11:44 -0300

    Ex 70 finalizado

--- Mundo 2/ex070.py
+++ Mundo 2/ex070.py
@@ -4,18 +4,23 @@
 # B) quantos produtos custam mais de R$1000.
 # C) qual é o nome do produto mais barato.
 
-tot = mil = 0
+tot = mil = menor = cont = 0
+barato = ''
 while True:
     produto = str(input('Nome do produto: '))
     preço = float(input('Preço: R$'))
+    cont +=1
     tot += preço
     if preço > 1000:
         mil += 1
-
+    if cont == 1 or preço < menor:
+        menor = preço
+        barato = produto
     resp = ' '
     while resp not in 'SN':
             resp = str(input('Deseja continuar: [S/N] ')).upper().strip()[0]
     if resp == 'N':
         break
 print(f'O total gasto foi de R${tot:.2f}.')
-print(f'Temos {mil} produtos custando mais de mil reais.')
+print(f'Temos {mil} produtos custando mais de R$1000.00 reais.')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}.')
