42834e2acc0246e29ba74c51fa887bc8a9d8f8c9
Gleise Silva <gleise.silva1818@gmail.com>
2026-09-24 21:52:29 -0300

    Produto e preço

--- Mundo 2/ex070.py
+++ Mundo 2/ex070.py
@@ -4,3 +4,15 @@
 # B) quantos produtos custam mais de R$1000.
 # C) qual é o nome do produto mais barato.
 
+while True:
+    produto = str(input('Nome do produto: '))
+    preço = float(input('Preço: R$'))
+
+
+
+    resp = ' '
+    while resp not in 'SN':
+            resp = str(input('Deseja continuar: [S/N] ')).upper().strip()[0]
+    if resp == 'N':
+        break
+print('ACABOU')
