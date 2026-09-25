7dd07c4eed907c1fe6031a8edb058251f2b2d17b
Gleise Silva <gleise.silva1818@gmail.com>
2026-09-25 14:41:48 -0300

    Mudança de cédula

--- Mundo 2/ex071.py
+++ Mundo 2/ex071.py
@@ -2,3 +2,26 @@
 # (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. OBS: Considere que o caixa possui
 # cédulas de R$50, R$20, R$10 e R$1.
 
+print('==' * 15)
+print('CAIXA ELETRÔNICO')
+print('==' * 15)
+valor = int(input('Que valor você quer sacar? R$'))
+total = valor
+cedula = 50
+totced = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R${cedula}.')
+        if cedula == 50: # -> mudança de cédula
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totced = 0
+        if total == 0:
+                break
